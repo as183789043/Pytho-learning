227c33d7616ad8420786688a536008faa98eda4d
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2022-04-14 16:33:17 +0800

    Update 網路爬蟲 Google圖書查詢.py

--- 網路爬蟲 Google圖書查詢.py
+++ 網路爬蟲 Google圖書查詢.py
@@ -5,6 +5,7 @@ url='https://www.googleapis.com/books/v1/volumes?maxResults=20&q=Python&projecti
 jsonfile='book.json'
 r=requests.get(url)
 r.encoding='utf-8'
-json_data=json.loads(r.text)
+json_data=json.loads(r.text) #loads 將json字串轉換成字典
 with open(jsonfile,'w+',encoding='utf8') as fp:
-    json.dump(json_data,fp,ensure_ascii=False, indent=2)
+    json.dump(json_data,fp,ensure_ascii=False, indent=2)   #dump 將字典轉換成json字串,裝進指定檔案   #ensure_ascii 輸出中文字(預設為True)     #indent 分行
+    
